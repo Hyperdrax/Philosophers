dfcd51417510efc9b6fd19ac13f4b5870c2dffe2
Florian Hensel <florianhensel@proton.me>
2026-06-15 23:14:27 +0200

    Implement thread management for philosophers

--- main.c
+++ main.c
@@ -1,5 +1,32 @@
 #include "philo.h"
 
+static int	start_threads(t_data *data)
+{
+	int			i;
+	pthread_t	mon;
+
+	data->start_time = get_time();
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		data->philos[i].last_meal = data->start_time;
+		if (pthread_create(&data->philos[i].thread, NULL,
+				routine, &data->philos[i]) != 0)
+			return (error_msg("Error: thread creation failed\n"));
+		i++;
+	}
+	if (pthread_create(&mon, NULL, monitor, data) != 0)
+		return (error_msg("Error: thread creation failed\n"));
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		pthread_join(data->philos[i].thread, NULL);
+		i++;
+	}
+	pthread_join(mon, NULL);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	data;
@@ -16,5 +43,11 @@ int	main(int argc, char **argv)
 		destroy_all(&data, data.nb_philo);
 		return (1);
 	}
+	if (start_threads(&data))
+	{
+		destroy_all(&data, data.nb_philo);
+		return (1);
+	}
+	destroy_all(&data, data.nb_philo);
 	return (0);
 }
